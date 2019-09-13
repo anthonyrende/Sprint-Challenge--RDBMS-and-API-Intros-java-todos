43cc6a2dca2f8ef0f7bb52f60aadf4e2907bdd7e
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-13 11:36:48 -0400

    Todo model

---
+++ src/main/java/com/lambdaschool/todo/model/Todo.java
@@ -0,0 +1,74 @@
+package com.lambdaschool.todo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "todo")
+public class Todo
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long todoid;
+
+    private String descrriptopn;
+    private double datetime;
+    private boolean completed;
+
+    @OneToMany(mappedBy = "todo",
+                cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("todo")
+    private List<User> userid = new ArrayList<>();
+
+    public Todo() {
+    }
+
+    public Todo(String descrriptopn, double datetime, boolean completed) {
+        this.descrriptopn = descrriptopn;
+        this.datetime = datetime;
+        this.completed = completed;
+    }
+
+    public long getTodoid() {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid) {
+        this.todoid = todoid;
+    }
+
+    public String getDescrriptopn() {
+        return descrriptopn;
+    }
+
+    public void setDescrriptopn(String descrriptopn) {
+        this.descrriptopn = descrriptopn;
+    }
+
+    public double getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(double datetime) {
+        this.datetime = datetime;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public List<User> getUserid() {
+        return userid;
+    }
+
+    public void setUserid(List<User> userid) {
+        this.userid = userid;
+    }
+}
